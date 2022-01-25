0c080f365deeef20e0150ce329c7e8b7497868a9
thebtype <91935629+thebtype@users.noreply.github.com>
2022-01-25 10:56:09 +0000

    fixes

--- Beacon of Light/script.js
+++ Beacon of Light/script.js
@@ -17,7 +17,7 @@ function init() {
     // create a scene and a camera
     scene = new THREE.Scene()
     scene.background = new THREE.Color(1,1,1)
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
+    camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 1000 )
     camera.position.y = - 100
 
     // create the renderer and add it to the html
