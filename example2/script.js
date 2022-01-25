88608d05ece0156957f96c0a96dec94ee2d8487d
thebtype <91935629+thebtype@users.noreply.github.com>
2022-01-25 09:10:17 +0000

    Assignment
    Assignment

--- example2/script.js
+++ example2/script.js
@@ -5,7 +5,7 @@ import { Rhino3dmLoader } from 'https://cdn.jsdelivr.net/npm/three@0.124.0/examp
 
 // declare variables to store scene, camera, and renderer
 let scene, camera, renderer
-const model = 'Rhino_Logo.3dm'
+const model = 'Beacon.3dm'
 
 // call functions
 init()
